1bd5a380df028b50e4ed54437d54ace36ffb78d8
Mszyca Stefan <szemranamszyca@yandex.ru>
2017-09-22 00:34:26 +0300

    window slide

--- my_img_manipulations.py
+++ my_img_manipulations.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import camera_calibration as my_cc
+import glob
 
 
 def abs_sobel(img, orient='x', thresh=(0,255)):
@@ -48,64 +49,97 @@ def direction_sobel(img, sobel_kernel = 3, thresh=(0, np.pi/2)):
     return binary_output
 
 
-def hls_select(img, thresh=(0, 255)):
+def color_select(img, sthresh=(0, 255), vtresh=(0,255)):
     hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
     s_channel = hls[:,:,2]
+    binary_output_s = np.zeros_like(s_channel)
+    binary_output_s[(s_channel > sthresh[0]) & (s_channel <= sthresh[1])] = 1
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    v_channel = hsv[:,:,2]
+    binary_output_v = np.zeros_like(v_channel)
+    binary_output_v[(v_channel > vtresh[0]) & (v_channel <= vtresh[1])] = 1
 
     binary_output = np.zeros_like(s_channel)
-    binary_output[(s_channel > thresh[0]) & (s_channel <= thresh[1])] = 1
+    binary_output[(binary_output_s == 1) & (binary_output_v == 1)] = 1
     return binary_output
 
 def transform(img):
     # gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     img_size = (img.shape[1], img.shape[0])
 
-    left_top_src = [410,575]
-    right_top_src = [930, 575]
-    left_bottom_src= [280, 685]
-    right_bottom_src = [1110, 685]
+    img_width = img_size[0]
+    img_height = img_size[1]
 
-    offset = 25
-    left_top_dst = [offset, offset]
-    right_top_dst = [img_size[0]-offset, offset]
-    left_bottom_dst = [offset, img_size[1]-offset]
-    right_bottom_dst = [img_size[0]-offset, img_size[1]-offset]
+    bottom_width_pct = .76
+    bottom_width = img_width * bottom_width_pct
+
+    top_width_pct = .08
+    top_width = img_width * top_width_pct
+
+    top_trim = .62
+    bottom_trim = 0.935
+
+    left_top_src = [img_width/2 - top_width/2, img_height * top_trim]
+    right_top_src = [img_width/2 + top_width/2, img_height * top_trim]
+    left_bottom_src= [img_width/2 - bottom_width/2, img_height * bottom_trim]
+    right_bottom_src = [img_width/2 + bottom_width/2, img_height * bottom_trim]
+
+    offset = img_width*.20
+    left_top_dst = [offset, 0]
+    right_top_dst = [img_width-offset, 0]
+    left_bottom_dst = [offset, img_height]
+    right_bottom_dst = [img_width-offset, img_height]
 
     src = np.float32([left_top_src, right_top_src, right_bottom_src, left_bottom_src])
     dst = np.float32([left_top_dst, right_top_dst, right_bottom_dst, left_bottom_dst])
 
     M = cv2.getPerspectiveTransform(src, dst)
-
+    Minv = cv2.getPerspectiveTransform(dst, src)
     warped = cv2.warpPerspective(img, M, img_size)
-    return warped, M
+    return warped, M, Minv
 
 
+def pipeline(img):
+    objpoints, imgpoints = my_cc.camera_calibration()
+    undistored = my_cc.undistort(img, objpoints, imgpoints)
 
-img_name = 'test_images/test1.jpg'
-img = mpimg.imread(img_name)
+    sobelx = abs_sobel(undistored, 'x', (50, 255))
+    sobely = abs_sobel(undistored, 'y', (25, 255))
+    color_binary = color_select(undistored, sthresh=(100, 255), vtresh=(50, 255))
 
-objpoints, imgpoints = my_cc.camera_calibration()
-undistored = my_cc.undistort(img, objpoints, imgpoints)
+    combined = np.zeros_like(sobelx)
+    combined[((sobelx == 1) & (sobely == 1) | (color_binary == 1))] = 1
 
+    transformed, M, Minv = transform(combined)
 
-mag_binary = gradmag_sobel(undistored, thresh=(100,200))
-dir_binary = direction_sobel(undistored, thresh=(np.pi/4, np.pi/2))
-color_binary = hls_select(undistored, thresh=(150, 255))
-
-combined = np.zeros_like(mag_binary)
-combined[(color_binary == 1) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+    return transformed
 
 
-
-transformed, M = transform(combined)
-
-# Plot the result
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
-f.tight_layout()
-ax1.imshow(undistored)
-ax1.set_title('Undistorted', fontsize=50)
-ax2.imshow(transformed, cmap='gray')
-ax2.set_title('Combined', fontsize=50)
-plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
-
-plt.show()
+# objpoints, imgpoints = my_cc.camera_calibration()
+# undistored = my_cc.undistort(img, objpoints, imgpoints)
+#
+# sobelx = abs_sobel(undistored, 'x', (50,255))
+# sobely = abs_sobel(undistored, 'y', (25, 255))
+#
+# mag_binary = gradmag_sobel(undistored, thresh=(150,200))
+# dir_binary = direction_sobel(undistored, thresh=(np.pi/4, np.pi/2))
+# color_binary = color_select(undistored, sthresh=(100, 255), vtresh=(50,255))
+#
+#
+# combined = np.zeros_like(mag_binary)
+# combined[((sobelx == 1)  &  (sobely == 1) | (color_binary == 1) )] = 1
+#
+#
+# transformed_nobinary, M_nb, Minv_mb = transform(undistored)
+# transformed, M, Minv = transform(combined)
+#
+# f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+# f.tight_layout()
+# ax1.imshow(transformed_nobinary)
+# ax1.set_title('Undistorted', fontsize=50)
+# ax2.imshow(transformed, cmap='gray')
+# ax2.set_title('Combined', fontsize=50)
+# plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+#
+# plt.show()
